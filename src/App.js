05364cd31d4247734237cf7d53ec4485ccd880b6
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2024-09-01 00:17:41 +0700

    multi lan add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  // const { t, i18n } = useTranslation();
+  // const changeLanguage = (lng) => {
+  //   i18n.changeLanguage(lng);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <header className="App-header">
+        <select onChange={(e) => changeLanguage(e.target.value)}>
+          <option value="en">English</option>
+          <option value="vi">Tiếng Việt</option>
+          <option value="zh">中文</option>
+        </select>
+        <h1>{t("welcome")}</h1>
+        <p>{t("description")}</p>
+        <p>{t("dick")}</p>
+      </header> */}
     </div>
   );
 }
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Flex, Layout } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+const headerStyle = {
+    textAlign: "center",
+    color: "#fff",
+    height: 64,
+    paddingInline: 0,
+    lineHeight: "64px",
+    backgroundColor: "black",
+    position: "fixed",
+    width:'100%',
+    top:0
+  };
+  
+  const contentStyle = {
+    textAlign: "center",
+    minHeight: 120,
+    lineHeight: "120px",
+    color: "#fff",
+    backgroundColor: "#0958d9",
+  };
+  
+  const siderStyle = {
+    textAlign: "center",
+    lineHeight: "120px",
+    color: "#fff",
+    backgroundColor: "#1677ff",
+  };
+  
+  const footerStyle = {
+    textAlign: "center",
+    color: "#fff",
+    backgroundColor: "#4096ff",
+  };
+  
+  const layoutStyle = {
+    overflow: "hidden",
+  };
+
+const Layout = () => {
+  return (
+    <div>
+              <Layout style={layoutStyle}>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </div>
+  )
+}
+
+export default Layout
